d9d1f86fa4fd3a2f618a0597777a6cdcef26ca28
Daniel A. <danielasefa975@gmail.com>
2026-03-27 20:43:56 -0600

    Removed .pyc, & printed table from JSON

--- src/ipv4TableSelection.py
+++ src/ipv4TableSelection.py
@@ -1,37 +1,76 @@
 import json
 import os
-import pprint
 
 # Author: Daniel Asefa
+# Date: 3-27-2026
+# Purpose: take JSON data and display it in a table
 
-# Lets think about this
-# displayIPv4Table() should call getJsonData()
-# Then come up with a fancy line to print each line
+class CidrData:
+    def __init__(self, data: dict):
+        self.id = data["id"]
+        self.cidr = data["cidr"]
+        self.subnetMask = data["subnetMask"]
+        self.blockSize = data["blockSize"]
+        self.numberOfSubnets = data["numberOfSubnets"]
+        self.hosts = data["hosts"]
+
+    # No need for setter, this is hard coded data that never changes
+
+    # getters
+    def getId(self):
+        return self.id
+    
+    def getCidr(self):
+        return self.cidr
+
+    def getSubnetMask(self):
+        return self.subnetMask
+    
+    def getBlockSize(self):
+        return self.blockSize
+    
+    def getNumberOfSubnets(self):
+        return self.numberOfSubnets
+    
+    def getHosts(self):
+        return self.hosts
+    
+    # Str method
+    def __str__(self):
+        return (f"{self.id}\t {self.cidr}\t\t {self.subnetMask}\t {self.blockSize}\t {self.numberOfSubnets}\t {self.hosts}")
+
+    def showData(self):
+        return (f"{self.cidr:>3}\t {self.subnetMask:<15}\t {self.blockSize:>3}\t {str(self.numberOfSubnets):>5}\t\t {self.hosts:>10,}")
 
 
 
-
-
-
-def displayIPv4Table():
-    print("\n IPv4 NETWORKING TABLE\n"
-    "CIDR\tSubnetMask\tBlock Size\t # of Hosts\t Hosts\n"
-    )
-
+def displayJsonData():
     # Path for json file location
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     data_path = os.path.join(base_dir, "data", "data.json")
 
-
     # load the JSON data
     with open(data_path, 'r') as f:
-        data = json.load(f)
+        json_data = json.load(f)
     
-    # Print data
+    cidrList = [CidrData(entry) for entry in json_data]
 
-    pprint.pprint(data)
+    for cidr in cidrList:
+        print(cidr.showData())
+
+    f.close()
 
 
 
+def displayIPv4Table():
+    title = str("IPv4 NETWORKING TABLE")
+    line = "--------------------------------------------------------------------"
+    print(f"\n{line}\n{title:^68}\n{line}\n"
+    "CIDR\tSubnetMask         Block Size\t # of Subnets\t     Hosts\n"
+    f"{line}")
+
+    displayJsonData()
+    
+    print(f"{line}")
 
     input("\nPress any button to continue to main menu")
