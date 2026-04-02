a73fae6b5ea0188f5e3a4aef95f4ca311cce7b78
Daniel A. <danielasefa975@gmail.com>
2026-04-02 10:06:21 -0600

    Changed inputs to a Dictionary

--- src/main_pages/random_selection.py
+++ src/main_pages/random_selection.py
@@ -12,29 +12,38 @@ from src.utils.data_formatter import format_json_data
 def check_answers():
     # check_answer() takes 3 arguments
         # question_type         This determines the answer response
+            # 1. nc_cidr    = This indicates it is a question that gives CIDR, is a class A/B, and asks for all data except hosts 
+            # 2. nc_subnet  = This indicates it is a question that gives Subnet, is a class A/B, and asks for all data except hosts
+            # 3. yc_cidr    = This indicates it is a question that gives CIDR, is a class C, and asks for all other data
+            # 4. yc_subnet  = This indicates it is a question that gives Subnet, is a class C, and asks for all other data 
+            # 5. yc_hosts   = This indicates it is a question that gives hosts, is a class C, and asks for all other data
+
         # question_data_list    This is the static answer data compared entered data is compared to
         # entered_data_list     This is the user data that was entered
     print("do it")
     return
 
-def not_type_c_question(question_data_list: list, question_not_c: int) -> str:
+def not_type_c_question(question_data_list: list, question_not_c: int) -> str:    
+    # Match entered data to question
     match question_not_c:
         case 1:
             print(f"For a CIDR of {question_data_list[0]} provide the following:\n")
-            entered_ipv4_class = input(f"IPv4 Class\t\t: ")
-            entered_subnet_mask = input(f"Subnet Mask\t\t: ")
-            entered_block_size = input(f"Block Size\t\t: ")
-            entered_num_of_subnets = input(f"Number of Subnets\t: ")
-
+            entered_data_dict = {
+                "ipv4_class": input(f"IPv4 Class\t\t: "),
+                "subnet_mask": input(f"Subnet Mask\t\t: "),
+                "block_size": input(f"Block Size\t\t: "),
+                "num_of_subnets": input(f"Number of Subnets\t: ")
+            }
             # Execute method to compare inputted data to answers
             check_answers()
         case 2:
             print(f"For a Subnet Mask of {question_data_list[1]} provide the following:\n")
-            entered_ipv4_class = input(f"IPv4 Class\t\t: ")
-            entered_cidr = input(f"CIDR\t\t\t\t: ")
-            entered_block_size = input(f"Block Size\t\t: ")
-            entered_num_of_subnets = input(f"Number of Subnets\t: ")
-
+            entered_data_dict = {
+                "ipv4_class": input(f"IPv4 Class\t\t: "),
+                "cidr": input(f"CIDR\t\t\t\t: "),
+                "block_size": input(f"Block Size\t\t: "),
+                "num_of_subnets": input(f"Number of Subnets\t: ")
+            }
             # Execute method to compare inputted data to answers
             check_answers()
         case __:
@@ -48,33 +57,36 @@ def type_c_question(question_data_list: list, question_c: int) -> str:
     match question_c:
         case 1:
             print(f"For a CIDR of {question_data_list[0]} provide the following:\n")
-            entered_ipv4_class = input(f"IPv4 Class\t\t: ")
-            entered_subnet_mask = input(f"Subnet Mask\t\t: ")
-            entered_block_size = input(f"Block Size\t\t: ")
-            entered_num_of_subnets = input(f"Number of Subnets\t: ")
-            entered_num_of_hosts = input(f"Number of Hosts\t: ")
-
+            entered_data_dict = {
+                "ipv4_class": input(f"IPv4 Class\t\t: "),
+                "subnet_mask": input(f"Subnet Mask\t\t: "),
+                "block_size": input(f"Block Size\t\t: "),
+                "num_of_subnets": input(f"Number of Subnets\t: "),
+                "num_of_hosts": input(f"Number of Hosts\t: ")
+            }
             # Execute method to compare inputted data to answers
             check_answers()
         case 2:
             print(f"For a Subnet Mask of {question_data_list[1]} provide the following:\n")
-            entered_ipv4_class = input(f"IPv4 Class\t\t: ")
-            entered_cidr = input(f"CIDR\t\t\t\t: ")
-            entered_block_size = input(f"Block Size\t\t: ")
-            entered_num_of_subnets = input(f"Number of Subnets\t: ")
-            entered_num_of_hosts = input(f"Number of Hosts\t: ")
-
+            entered_data_dict = {
+                "ipv4_class": input(f"IPv4 Class\t\t: "),
+                "cidr": input(f"CIDR\t\t\t\t: "),
+                "block_size": input(f"Block Size\t\t: "),
+                "num_of_subnets": input(f"Number of Subnets\t: "),
+                "num_of_hosts": input(f"Number of Hosts\t: ")
+            }
             # Execute method to compare inputted data to answers
             check_answers()
         case 3:
             print(f"For a number of hosts of {question_data_list[2]} provide the following:\n")
-            entered_ipv4_class = input(f"IPv4 Class\t\t: ")
-            entered_cidr = input(f"CIDR\t\t\t\t: ")
-            entered_subnet_mask = input(f"Subnet Mask\t\t: ")
-            entered_block_size = input(f"Block Size\t\t: ")
-            entered_num_of_subnets = input(f"Number of Subnets\t: ")
-            entered_num_of_hosts = input(f"Number of Hosts\t: ")
-
+            entered_data_dict = {
+                "ipv4_class": input(f"IPv4 Class\t\t: "),
+                "cidr": input(f"CIDR\t\t\t\t: "),
+                "subnet_mask": input(f"Subnet Mask\t\t: "),
+                "block_size": input(f"Block Size\t\t: "),
+                "num_of_subnets": input(f"Number of Subnets\t: "),
+                "num_of_hosts": input(f"Number of Hosts\t: ")
+            }
             # Execute method to compare inputted data to answers
             check_answers()
         case __:
@@ -90,6 +102,7 @@ def is_id_c(selection_id: int) -> bool:
         return False
 
 def random_selection():
+    global entered_data_dict
     cidr_list = format_json_data()              # Generate list from JSON data 
     selection_id = int(random.randint(1,24))    # Random number from 1 to 24 referencing id of JSON data
     question_c = random.randint(1,3)            # Question can give user one of 3 variables (CIDR / SubnetMask / Hosts)
@@ -105,6 +118,9 @@ def random_selection():
             question_data_list.append(cidr.getBlockSize())           # question_data_list[3] = Block Size
             question_data_list.append(cidr.getNumberOfSubnets())     # question_data_list[4] = Number of subnets
             question_data_list.append(cidr.getHosts())               # question_data_list[5] = Number of Hosts
+
+    # Empty dictionary for new question (for now)
+    entered_data_dict = {}
 
     if is_id_c(selection_id):
         type_c_question(question_data_list, question_c)
