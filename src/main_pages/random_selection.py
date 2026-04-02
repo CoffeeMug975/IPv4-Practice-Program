c8e3c4720cc12fa085a4ec9d49ff2888443c357c
Daniel A. <danielasefa975@gmail.com>
2026-04-01 21:00:25 -0600

    changed var name & defined check_answer()

--- src/main_pages/random_selection.py
+++ src/main_pages/random_selection.py
@@ -9,37 +9,45 @@ from src.utils.data_formatter import format_json_data
 # Is this much case match good? Look into a better way
 # Have this page loop until user enters "back"
 
-def not_type_c_question(question_list: list, question_not_c: int) -> str:
+def check_answers():
+    # check_answer() takes 3 arguments
+        # question_type         This determines the answer response
+        # question_data_list    This is the static answer data compared entered data is compared to
+        # entered_data_list     This is the user data that was entered
+    print("do it")
+    return
+
+def not_type_c_question(question_data_list: list, question_not_c: int) -> str:
     match question_not_c:
         case 1:
-            print(f"For a CIDR of {question_list[0]} provide the following:\n")
+            print(f"For a CIDR of {question_data_list[0]} provide the following:\n")
             entered_ipv4_class = input(f"IPv4 Class: ")
             entered_subnet_mask = input(f"Subnet Mask: ")
             entered_block_size = input(f"Block Size: ")
             entered_num_of_subnets = input(f"Number of Subnets: ")
 
             # Execute method to compare inputted data to answers
-            # check_answers()
+            check_answers()
         case 2:
-            print(f"For a Subnet Mask of {question_list[1]} provide the following:\n")
+            print(f"For a Subnet Mask of {question_data_list[1]} provide the following:\n")
             entered_ipv4_class = input(f"IPv4 Class: ")
             entered_cidr = input(f"CIDR: ")
             entered_block_size = input(f"Block Size: ")
             entered_num_of_subnets = input(f"Number of Subnets: ")
 
             # Execute method to compare inputted data to answers
-            # check_answers()
+            check_answers()
         case __:
             print("Error: invalid question type of {question_not_c}")
     return
 
 
-def type_c_question(question_list: list, question_c: int) -> str:
-    print(f"Question List: {question_list}")
+def type_c_question(question_data_list: list, question_c: int) -> str:
+    print(f"Question List: {question_data_list}")
     print(f"Question Type: {question_c}")
     match question_c:
         case 1:
-            print(f"For a CIDR of {question_list[0]} provide the following:\n")
+            print(f"For a CIDR of {question_data_list[0]} provide the following:\n")
             entered_ipv4_class = input(f"IPv4 Class: ")
             entered_subnet_mask = input(f"Subnet Mask: ")
             entered_block_size = input(f"Block Size: ")
@@ -47,9 +55,9 @@ def type_c_question(question_list: list, question_c: int) -> str:
             entered_num_of_hosts = input(f"Number of Hosts: ")
 
             # Execute method to compare inputted data to answers
-            # check_answers()
+            check_answers()
         case 2:
-            print(f"For a Subnet Mask of {question_list[1]} provide the following:\n")
+            print(f"For a Subnet Mask of {question_data_list[1]} provide the following:\n")
             entered_ipv4_class = input(f"IPv4 Class: ")
             entered_cidr = input(f"CIDR: ")
             entered_block_size = input(f"Block Size: ")
@@ -57,9 +65,9 @@ def type_c_question(question_list: list, question_c: int) -> str:
             entered_num_of_hosts = input(f"Number of Hosts: ")
 
             # Execute method to compare inputted data to answers
-            # check_answers()
+            check_answers()
         case 3:
-            print(f"For a number of hosts of {question_list[2]} provide the following:\n")
+            print(f"For a number of hosts of {question_data_list[2]} provide the following:\n")
             entered_ipv4_class = input(f"IPv4 Class: ")
             entered_cidr = input(f"CIDR: ")
             entered_subnet_mask = input(f"Subnet Mask: ")
@@ -68,7 +76,7 @@ def type_c_question(question_list: list, question_c: int) -> str:
             entered_num_of_hosts = input(f"Number of Hosts: ")
 
             # Execute method to compare inputted data to answers
-            # check_answers()
+            check_answers()
         case __:
             print("Error: invalid question type of {question_not_c}")
 
@@ -90,20 +98,20 @@ def random_selection():
     # Get question data for given selection id and store it in a list
     for cidr in cidr_list:
         if selection_id == cidr.getId():
-            global question_list
-            question_list = [cidr.getCidr()]                    # question_list[0] = CIDR
-            question_list.append(cidr.getIpClass())             # question_list[1] = IPv4 Class
-            question_list.append(cidr.getSubnetMask())          # question_list[2] = Subnet Mask
-            question_list.append(cidr.getBlockSize())           # question_list[3] = Block Size
-            question_list.append(cidr.getNumberOfSubnets())     # question_list[4] = Number of subnets
-            question_list.append(cidr.getHosts())               # question_list[5] = Number of Hosts
+            global question_data_list
+            question_data_list = [cidr.getCidr()]                    # question_data_list[0] = CIDR
+            question_data_list.append(cidr.getIpClass())             # question_data_list[1] = IPv4 Class
+            question_data_list.append(cidr.getSubnetMask())          # question_data_list[2] = Subnet Mask
+            question_data_list.append(cidr.getBlockSize())           # question_data_list[3] = Block Size
+            question_data_list.append(cidr.getNumberOfSubnets())     # question_data_list[4] = Number of subnets
+            question_data_list.append(cidr.getHosts())               # question_data_list[5] = Number of Hosts
 
     if is_id_c(selection_id):
-        type_c_question(question_list, question_c)
+        type_c_question(question_data_list, question_c)
         return
     
     if not is_id_c(selection_id):
-        not_type_c_question(question_list, question_not_c)
+        not_type_c_question(question_data_list, question_not_c)
         return
 
     input ("Press enter to exit random test env.")
