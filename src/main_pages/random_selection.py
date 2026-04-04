7c73a53dd0479a07c8446b641f2794294daa450a
Daniel A. <danielasefa975@gmail.com>
2026-04-04 08:45:31 -0600

    data method changes

--- src/main_pages/random_selection.py
+++ src/main_pages/random_selection.py
@@ -9,25 +9,28 @@ from src.utils.data_formatter import format_json_data
 # Is this much case match good? Look into a better way
 # Have this page loop until user enters "back"
 
-def check_answers():
+def check_answers(question_type: str, question_data_dict: dict, entered_data_dict: dict) -> str:
     # check_answer() takes 3 arguments
         # question_type         This determines the answer response
-            # 1. nc_cidr    = This indicates it is a question that gives CIDR, is a class A/B, and asks for all data except hosts 
-            # 2. nc_subnet  = This indicates it is a question that gives Subnet, is a class A/B, and asks for all data except hosts
-            # 3. yc_cidr    = This indicates it is a question that gives CIDR, is a class C, and asks for all other data
-            # 4. yc_subnet  = This indicates it is a question that gives Subnet, is a class C, and asks for all other data 
-            # 5. yc_hosts   = This indicates it is a question that gives hosts, is a class C, and asks for all other data
+            #    Can make this a string + ensure that string is validated for security (is this needed?)
+            # 1. nc_cidr    = This indicates it is a question that gives CIDR, is a class A/B, and asks for all data except hosts   Done 
+            # 2. nc_subnet  = This indicates it is a question that gives Subnet, is a class A/B, and asks for all data except hosts Done
+            # 3. yc_cidr    = This indicates it is a question that gives CIDR, is a class C, and asks for all other data            Done
+            # 4. yc_subnet  = This indicates it is a question that gives Subnet, is a class C, and asks for all other data          d
+            # 5. yc_hosts   = This indicates it is a question that gives hosts, is a class C, and asks for all other data           d
 
-        # entered_data_dict    This is the static answer data compared entered data is compared to
-        # entered_data_list     This is the user data that was entered
+        # question_data_dict    This is the static answer data compared entered data is compared to
+        # entered_data_dict     This is the user data that was entered
     print("do it")
-    return
+    return "string"
 
-def not_type_c_question(question_data_list: list, question_not_c: int) -> str:    
+def not_type_c_question(question_data_dict: dict, question_not_c: int):    
     # Match entered data to question
+    print(f"Question Data: {question_data_dict}")
     match question_not_c:
         case 1:
-            print(f"For a CIDR of {question_data_list[0]} provide the following:\n")
+            question_type = str("nc_cidr")
+            print(f"For a CIDR of {question_data_dict["cidr"]} provide the following:\n")
             entered_data_dict = {
                 "ipv4_class": input(f"IPv4 Class\t\t: "),
                 "subnet_mask": input(f"Subnet Mask\t\t: "),
@@ -37,7 +40,8 @@ def not_type_c_question(question_data_list: list, question_not_c: int) -> str:
             # Execute method to compare inputted data to answers
             check_answers()
         case 2:
-            print(f"For a Subnet Mask of {question_data_list[1]} provide the following:\n")
+            question_type = str("nc_subnet")
+            print(f"For a Subnet Mask of {question_data_dict["subnet_mask"]} provide the following:\n")
             entered_data_dict = {
                 "ipv4_class": input(f"IPv4 Class\t\t: "),
                 "cidr": input(f"CIDR\t\t\t\t: "),
@@ -51,47 +55,51 @@ def not_type_c_question(question_data_list: list, question_not_c: int) -> str:
     return
 
 
-def type_c_question(question_data_list: list, question_c: int) -> str:
+def type_c_question(question_data_dict: dict, question_c: int):
     # Match entered data to question
+    print(f"Question Data: {question_data_dict}")
     match question_c:
         case 1:
-            print(f"For a CIDR of {question_data_list[0]} provide the following:\n")
+            question_type = str("yc_cidr")
+            print(f"For a CIDR of {question_data_dict["cidr"]} provide the following:\n")
             entered_data_dict = {
                 "ipv4_class": input(f"IPv4 Class\t\t: "),
                 "subnet_mask": input(f"Subnet Mask\t\t: "),
                 "block_size": input(f"Block Size\t\t: "),
                 "num_of_subnets": input(f"Number of Subnets\t: "),
-                "num_of_hosts": input(f"Number of Hosts\t: ")
+                "num_of_hosts": input(f"Number of Hosts\t\t: ")
             }
             # Execute method to compare inputted data to answers
             check_answers()
         case 2:
-            print(f"For a Subnet Mask of {question_data_list[1]} provide the following:\n")
+            question_type = str("yc_subnet")
+            print(f"For a Subnet Mask of {question_data_dict["subnet_mask"]} provide the following:\n")
             entered_data_dict = {
                 "ipv4_class": input(f"IPv4 Class\t\t: "),
                 "cidr": input(f"CIDR\t\t\t\t: "),
                 "block_size": input(f"Block Size\t\t: "),
                 "num_of_subnets": input(f"Number of Subnets\t: "),
-                "num_of_hosts": input(f"Number of Hosts\t: ")
+                "num_of_hosts": input(f"Number of Hosts\t\t: ")
             }
             # Execute method to compare inputted data to answers
             check_answers()
         case 3:
-            print(f"For a number of hosts of {question_data_list[2]} provide the following:\n")
+            question_type = str("yc_hosts")
+            print(f"For a number of hosts of {question_data_dict["hosts"]} provide the following:\n")
             entered_data_dict = {
                 "ipv4_class": input(f"IPv4 Class\t\t: "),
                 "cidr": input(f"CIDR\t\t\t\t: "),
                 "subnet_mask": input(f"Subnet Mask\t\t: "),
                 "block_size": input(f"Block Size\t\t: "),
                 "num_of_subnets": input(f"Number of Subnets\t: "),
-                "num_of_hosts": input(f"Number of Hosts\t: ")
+                "num_of_hosts": input(f"Number of Hosts\t\t: ")
             }
             # Execute method to compare inputted data to answers
             check_answers()
         case __:
             print("Error: invalid question type of {question_not_c}")
 
-    return "something"
+    return
 
 
 def is_id_c(selection_id: int) -> bool:
@@ -103,6 +111,11 @@ def is_id_c(selection_id: int) -> bool:
 def random_selection():
     global entered_data_dict
     global question_data_dict
+
+    # Empty dictionary for new question (for now)
+    entered_data_dict = {}
+    question_data_dict = {}
+    
     cidr_list = format_json_data()              # Generate list from JSON data 
     selection_id = int(random.randint(1,24))    # Random number from 1 to 24 referencing id of JSON data
     question_c = random.randint(1,3)            # Question can give user one of 3 variables (CIDR / SubnetMask / Hosts)
@@ -119,22 +132,12 @@ def random_selection():
                 "num_of_subnets": cidr.getNumberOfSubnets(),
                 "hosts": cidr.getHosts()
             }
-            # question_data_list = [cidr.getCidr()]                    # question_data_list[0] = CIDR
-            # question_data_list.append(cidr.getIpClass())             # question_data_list[1] = IPv4 Class
-            # question_data_list.append(cidr.getSubnetMask())          # question_data_list[2] = Subnet Mask
-            # question_data_list.append(cidr.getBlockSize())           # question_data_list[3] = Block Size
-            # question_data_list.append(cidr.getNumberOfSubnets())     # question_data_list[4] = Number of subnets
-            # question_data_list.append(cidr.getHosts())               # question_data_list[5] = Number of Hosts
 
-    # Empty dictionary for new question (for now)
-    entered_data_dict = {}
 
     if is_id_c(selection_id):
         type_c_question(question_data_dict, question_c)
-        return
     
     if not is_id_c(selection_id):
         not_type_c_question(question_data_dict, question_not_c)
-        return
 
     input ("Press enter to exit random test env.")
